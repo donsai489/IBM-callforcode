afe63a79bd5eb8fc4c0d185eeca40cfc3cfa03e9
donsai489 <60974735+donsai489@users.noreply.github.com>
2020-07-31 10:40:21 +0530

    Create Objectdetection.py

---
+++ Objectdetection.py
@@ -0,0 +1,17 @@
+#this code runs in python3 enviroinment
+import numpy as np
+import argparse
+import time
+import cv2
+import os
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True,
+help="path to input image")
+ap.add_argument("-y", "--yolo", required=True,
+help="base path to YOLO directory")
+ap.add_argument("-c", "--confidence", type=float, default=0.5,
+help="minimum probability to filter weak detections")
+ap.add_argument("-t", "--threshold", type=float, default=0.3,
+help="threshold when applying non-maxima suppression")
+args = vars(ap.parse_args())
